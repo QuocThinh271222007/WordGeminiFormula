a62c77118e955606f9c1395ddd7e80e710ce94df
QuocThinh271222007 <150253833+QuocThinh271222007@users.noreply.github.com>
2026-08-31 14:19:44 +0700

    fix: match Office COM interface ABI

--- src/WordGeminiFormula.AddIn/Interop/OfficeInterfaces.cs
+++ src/WordGeminiFormula.AddIn/Interop/OfficeInterfaces.cs
@@ -1,9 +1,11 @@
 using System;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 namespace WordGeminiFormula.AddIn.Interop
 {
-    public enum ExtConnectMode
+    [Guid("289E9AF1-4973-11D1-AE81-00A0C90F26F4")]
+    public enum ExtConnectMode : uint
     {
         AfterStartup = 0,
         Startup = 1,
@@ -13,7 +15,8 @@ namespace WordGeminiFormula.AddIn.Interop
         UISetup = 5
     }
 
-    public enum ExtDisconnectMode
+    [Guid("289E9AF2-4973-11D1-AE81-00A0C90F26F4")]
+    public enum ExtDisconnectMode : uint
     {
         HostShutdown = 0,
         UserClosed = 1,
@@ -21,34 +24,53 @@ namespace WordGeminiFormula.AddIn.Interop
         SolutionClosed = 3
     }
 
-    [ComVisible(true)]
+    // ABI-compatible declaration of Extensibility.IDTExtensibility2.
+    // Keep this in sync with the Office/Extensibility type library rather than
+    // exposing a custom dual interface; Word invokes this interface through COM.
+    [ComImport]
     [Guid("B65AD801-ABAF-11D0-BB8B-00A0C90F2744")]
-    [InterfaceType(ComInterfaceType.InterfaceIsDual)]
+    [TypeLibType((short)4160)]
     public interface IDTExtensibility2
     {
         [DispId(1)]
-        void OnConnection([In, MarshalAs(UnmanagedType.IDispatch)] object application,
+        [MethodImpl(MethodImplOptions.InternalCall, MethodCodeType = MethodCodeType.Runtime)]
+        void OnConnection(
+            [In, MarshalAs(UnmanagedType.IDispatch)] object application,
             [In] ExtConnectMode connectMode,
             [In, MarshalAs(UnmanagedType.IDispatch)] object addInInst,
-            [In] ref Array custom);
+            [In, MarshalAs(UnmanagedType.SafeArray, SafeArraySubType = VarEnum.VT_VARIANT)] ref Array custom);
 
         [DispId(2)]
-        void OnDisconnection([In] ExtDisconnectMode removeMode, [In] ref Array custom);
+        [MethodImpl(MethodImplOptions.InternalCall, MethodCodeType = MethodCodeType.Runtime)]
+        void OnDisconnection(
+            [In] ExtDisconnectMode removeMode,
+            [In, MarshalAs(UnmanagedType.SafeArray, SafeArraySubType = VarEnum.VT_VARIANT)] ref Array custom);
+
         [DispId(3)]
-        void OnAddInsUpdate([In] ref Array custom);
+        [MethodImpl(MethodImplOptions.InternalCall, MethodCodeType = MethodCodeType.Runtime)]
+        void OnAddInsUpdate(
+            [In, MarshalAs(UnmanagedType.SafeArray, SafeArraySubType = VarEnum.VT_VARIANT)] ref Array custom);
+
         [DispId(4)]
-        void OnStartupComplete([In] ref Array custom);
+        [MethodImpl(MethodImplOptions.InternalCall, MethodCodeType = MethodCodeType.Runtime)]
+        void OnStartupComplete(
+            [In, MarshalAs(UnmanagedType.SafeArray, SafeArraySubType = VarEnum.VT_VARIANT)] ref Array custom);
+
         [DispId(5)]
-        void OnBeginShutdown([In] ref Array custom);
+        [MethodImpl(MethodImplOptions.InternalCall, MethodCodeType = MethodCodeType.Runtime)]
+        void OnBeginShutdown(
+            [In, MarshalAs(UnmanagedType.SafeArray, SafeArraySubType = VarEnum.VT_VARIANT)] ref Array custom);
     }
 
-    [ComVisible(true)]
+    // ABI-compatible declaration of Microsoft.Office.Core.IRibbonExtensibility.
+    [ComImport]
     [Guid("000C0396-0000-0000-C000-000000000046")]
-    [InterfaceType(ComInterfaceType.InterfaceIsDual)]
+    [TypeLibType((short)0x1040)]
     public interface IRibbonExtensibility
     {
         [DispId(1)]
         [return: MarshalAs(UnmanagedType.BStr)]
-        string GetCustomUI([MarshalAs(UnmanagedType.BStr)] string ribbonId);
+        [MethodImpl(MethodImplOptions.InternalCall, MethodCodeType = MethodCodeType.Runtime)]
+        string GetCustomUI([In, MarshalAs(UnmanagedType.BStr)] string ribbonId);
     }
 }
